636a7b244bc8e4b1f7b6365a25a7f205251c39c4
gaw3568 <40763988+gaw3568@users.noreply.github.com>
2022-09-16 17:30:11 +0900

    [Bronze V] Title: 학점계산, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,36 @@
+score = input()
+
+if score[0] == 'A':
+    if score[1] == '+':
+        print(4.3)
+    elif score[1] == '0':
+        print(4.0)
+    elif score[1] == '-':
+        print(3.7)
+
+elif score[0] == 'B':
+    if score[1] == '+':
+        print(3.3)
+    elif score[1] == '0':
+        print(3.0)
+    elif score[1] == '-':
+        print(2.7)
+
+elif score[0] == 'C':
+    if score[1] == '+':
+        print(2.3)
+    elif score[1] == '0':
+        print(2.0)
+    elif score[1] == '-':
+        print(1.7)
+
+elif score[0] == 'D':
+    if score[1] == '+':
+        print(1.3)
+    elif score[1] == '0':
+        print(1.0)
+    elif score[1] == '-':
+        print(0.7)
+
+elif score[0] == 'F':
+    print(0.0)
