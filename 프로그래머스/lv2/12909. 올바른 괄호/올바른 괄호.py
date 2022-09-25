d4311ba5f2a6730d1697b0ec2706a8750cf4ce6f
gaw3568 <40763988+gaw3568@users.noreply.github.com>
2022-09-25 15:15:10 +0900

    Update 올바른 괄호.py
    Add another solution

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -9,3 +9,17 @@ def solution(s):
             else:
                 arr.pop()
     return len(arr) == 0
+
+"""
+<다른방법>
+def is_pair(s):
+    x = 0
+    for w in s:
+        if x < 0:
+            break
+        if w == '(':
+            x = x + 1
+        elif w == ')':
+            x = x - 1 
+    return x == 0
+"""
