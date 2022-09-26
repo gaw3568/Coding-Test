3c265c1174b8c770ca11ed0949370f6608ac993b
gaw3568 <40763988+gaw3568@users.noreply.github.com>
2022-09-26 23:38:59 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,13 @@
+def solution(n, lost, reserve):
+    lost.sort()
+    reserve.sort()
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+    for r in _reserve:
+        f = r - 1
+        b = r + 1
+        if f in _lost:
+            _lost.remove(f)
+        elif b in _lost:
+            _lost.remove(b)
+    return n - len(_lost)
