0d0d49b8ca943fe2462666d27bde887d8298149c
gaw3568 <40763988+gaw3568@users.noreply.github.com>
2022-09-26 22:11:55 +0900

    [level 2] Title: 피보나치 수, Time: 22.41 ms, Memory: 13.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,10 @@
+def solution(n):
+    fibo_num = []
+    for i in range(n + 1):
+        if i == 0 or i == 1:
+            fibo_num.append(i)
+        else:
+            tmp_num = fibo_num[i-1] + fibo_num[i-2]
+            fibo_num.append(tmp_num % 1234567)
+    return fibo_num[-1]
+        
