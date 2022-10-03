14487f95b67046114205d6bd56ac99e53f9b195e
gaw3568 <40763988+gaw3568@users.noreply.github.com>
2022-10-03 22:28:08 +0900

    [unrated] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/README.md
+++ 프로그래머스/unrated/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [unrated] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10.3 MB, 시간: 0.00 ms
 
 ### 구분
 
--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,15 +1,7 @@
+import math
 def solution(denum1, num1, denum2, num2):
     denum_ = num1 * denum2 + num2 * denum1
     num_ = num1 * num2
+    gcd_num = math.gcd(denum_,num_)
     
-    a = denum_
-    b = num_
-    if a < b:
-        a, b = b, a
-    
-    while (a % b) != 0:
-        tmp_num = a % b
-        a = b
-        b = tmp_num
-    
-    return [denum_ // b, num_ // b]
+    return [denum_ // gcd_num, num_ // gcd_num]
