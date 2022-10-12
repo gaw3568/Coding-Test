9a9de8ee752526732ff81b74df36e450dfd4cc2f
gaw3568 <40763988+gaw3568@users.noreply.github.com>
2022-10-13 00:45:45 +0900

    [level 2] Title: 올바른 괄호, Time: 6.66 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,25 +1,13 @@
 def solution(s):
-    arr = []
+    cnt = 0
     for i in s:
-        if i == '(':
-            arr.append(i)
+        if i == "(":
+            cnt += 1
         else:
-            if arr == []:
-                return False
-            else:
-                arr.pop()
-    return len(arr) == 0
-
-"""
-<다른방법>
-def is_pair(s):
-    x = 0
-    for w in s:
-        if x < 0:
-            break
-        if w == '(':
-            x = x + 1
-        elif w == ')':
-            x = x - 1 
-    return x == 0
-"""
+            cnt -= 1
+        if cnt < 0:
+            return False
+    if cnt == 0:
+        return True
+    else:
+        return False
