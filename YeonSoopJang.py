b8fd845f40189de1fb91800a89d225001b67a257
gaw3568 <gaw3568@gmail.com>
2022-10-20 15:29:43 +0900

    Add 연습장

--- YeonSoopJang.py
+++ YeonSoopJang.py
@@ -1,5 +1,7 @@
 # 코드 연습장
 from collections import deque
+import sys
+from itertools import combinations
 
 def solution(arr1, arr2):
     answer = []
@@ -12,16 +14,3 @@ arr2 = [[3,4],[5,6]]
 print([x+y for x,y in zip([1,2,3],[3,4,5])])   
 print(solution(arr1=arr1, arr2=arr2))
 print("--------------------")
-
-N, K = map(int, input().split())
-
-a_list = list(map(int, input().split()))
-b_list = list(map(int, input().split()))
-
-a_list.sort()
-b_list.sort(reverse=True)
-
-for i in range(K):
-    if a_list[i] < b_list[i]:
-        a_list[i], b_list[i] = b_list[i], a_list[i]
-print(sum(a_list))
